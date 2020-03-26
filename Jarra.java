e3adcf2c873ef8a491cc28b829220997d1145eaa
EloxZ <45355879+EloxZ@users.noreply.github.com>
2020-03-26 11:57:33 +0100

    Update Jarra.java

--- Jarra.java
+++ Jarra.java
@@ -24,20 +24,20 @@ public Jarra(int capacidadInicial){
     }
     public int cantidad() {
     	return this.cantidad;
+    //David
+    public Object clone() throws CloneNotSupportedException
+      return (Jarra) super.clone;
+    }
+    public bool equals(Jarra otraJarra){
+      bool eqs = false;
+      if (null == otraJarra){ eqs = false;}
+      else if (this == otraJarra){ eqs = true;}
+      else if (otraJarra instanceOf Jarra.class){
+        eqs = this.capacidad == otraJarra capacidad &&
+          this.cantidad == otraJarra.cantidad;
+      } else { eqs = false;}
+    return eqs
+    }
+    //Fin David
   }
-  //David
-  public Object clone() throws CloneNotSupportedException
-    return (Jarra) super.clone;
-  }
-  public bool equals(Jarra otraJarra){
-    bool eqs = false;
-    if (null == otraJarra){ eqs = false;}
-    else if (this == otraJarra){ eqs = true;}
-    else if (otraJarra instanceOf Jarra.class){
-      eqs = this.capacidad == otraJarra capacidad &&
-        this.cantidad == otraJarra.cantidad;
-    } else { eqs = false;}
-  return eqs
-  }
-  //Fin David
 }
